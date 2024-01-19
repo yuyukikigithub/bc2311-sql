ef230180972e9ba57c59ab5bd073617400c3cee0
yuki.lau <szeki6578@gmail.com>
2024-01-19 16:30:10 +0800

    20240119

---
+++ BOOTCAMP_EXERCISE4.sql
@@ -0,0 +1,80 @@
+CREATE DATABASE BOOTCAMP_EXERCISE4;
+
+SHOW DATABASES;
+
+USE BOOTCAMP_EXERCISE4;
+
+create table players (
+player_id integer not null unique, 
+group_id integer not null
+);
+create table matches (
+	match_id integer not null unique, 
+    first_player integer not null, 
+    second_player integer not null, 
+    first_score integer not null, 
+    second_score integer not null
+);
+
+insert into players
+(player_id, group_id)
+values
+(20, 2),
+(30, 1),
+(40, 3),
+(45, 1),
+(50, 2),
+(65, 1);
+
+insert into matches
+(match_id , first_player , second_player , first_score , second_score)
+values
+(1, 30, 45, 10, 12),
+(2, 20, 50, 5, 5),
+(13, 65, 45, 10, 10),
+(5, 30, 65, 3, 15),
+(42, 45, 65, 8, 4);
+
+
+select p.group_id, max(m.score) as score from
+(select first_player as player, (first_score) as score from
+matches 
+union
+select second_player as player,(second_score) as score from
+matches ) m
+right join players p on m.player=p.player_id
+group by p.group_id
+order by p.group_id;
+
+
+(select first_player as player, (first_score) as score from
+matches 
+union
+select second_player as player,(second_score) as score from
+matches );
+
+select 
+-- j.row_num,
+j.gpId,
+j.pyId
+-- ,
+-- j.sum 
+from 
+(select row_number() over (
+ PARTITION BY p.group_id
+ ORDER BY n.sum desc
+ ) row_num,
+-- select 
+p.group_id as gpId,p.player_id as pyId,(n.sum) as sum from players p left join
+(select (m.player) as player, sum(m.score) as sum from 
+	(select first_player as player, (first_score) as score from
+	matches 
+	union
+	select second_player as player,(second_score) as score from
+	matches ) m
+group by m.player) n
+on p.player_id=n.player
+group by p.group_id,p.player_id) j
+where j.row_num=1
+;
+
